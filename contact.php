f0b90b9a46253a321ad9744b6ad59038bb8c1ebf
Mamatha Venukanti <117755651+Mamatha-Venukanti@users.noreply.github.com>
2023-10-09 20:26:52 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MPF Hotel-CONTACT</title>
+    <link  rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+    <?php require('inc/links.php'); ?>
+</head>
+<body class="bg-light">
+
+<?php require('inc/header.php'); ?>
+
+<div class="my-5 px-4">
+    <h2 class="fw-blod h-font text-center">CONTACT US</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+        WELCOME TO FM HOTEL YOU 
+        CAN GET ANY REQUIRED INFORMATION HERE!
+    </p>
+</div>
+
+<div align="center">
+<iframe
+    allow="microphone;"
+    width="850"
+    height="430"
+    src="https://console.dialogflow.com/api-client/demo/embedded/5c94fd20-1f93-4e38-a660-331e3c51eec0">
+</iframe>
+</div>
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+        <iframe class="w-100" height="320px"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15229.239911252722!2d78.32492124895994!3d17.396905535867692!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb945f8ece8141%3A0x11a988de812ad368!2sKokapet%2C%20Telangana!5e0!3m2!1sen!2sin!4v1686316318658!5m2!1sen!2sin" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+      </div>
+      <div class="col-lg-4 col-md-4">
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Call Us</h5>
+          <a href="tel: +919087965439" class="d-inline-block mb-2 text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i>+919087965439
+          </a>
+          <br>
+          <a href="tel: +919087965439" class="d-inline-block mb-2 text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i>+919087965439
+          </a>
+        </div>
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Follow Us</h5>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2">
+              <i class="bi bi-twitter"></i> Twitter
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2">
+              <i class="bi bi-instagram"></i> Instagram
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2">
+              <i class="bi bi-facebook"></i> Facebook
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php require('inc/footer.php');?>
+</body>
+</html>
